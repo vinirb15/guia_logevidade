56196afae0a21ee866b03a1167bce8ccf25d8b10
vbueno <viniciusbueno15@gmail.com>
2020-11-12 15:07:21 -0300

    logon add

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Logon from './pages/Logon';
+import Register from './pages/Register';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/logon" component={Logon} />
+                <Route path="/register" exact component={Register} />
             </Switch>
         </BrowserRouter>
     )
