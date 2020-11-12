f958fed9a73412102b802608dc02316900337f08
vbueno <viniciusbueno15@gmail.com>
2020-11-12 14:42:44 -0300

    home eddit

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import { FiMap, FiPhone, FiMail } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import './styles.css'
+
+import Header from '../../components/Header';
+
+export default function Profile() {
+
+    const [houses, setHouses] = useState([]);
+
+    // const houseId = localStorage.getItem('houseId');
+    // const houseName = localStorage.getItem('houseName');
+
+    useEffect(() => {
+        loadIndex()
+    }, []);
+
+    async function loadIndex(event) {
+        await api.get('/house').then(response => {
+            setHouses(response.data);
+            console.log(response)
+        })
+    }
+
+    async function searchHouse(event) {
+        try {
+            let val = event.target.value;
+            if (val === "") {
+                loadIndex()
+            }
+            const response = await api.get(`/find/${val}`);
+
+            setHouses(response.data)
+        } catch (error) {
+            loadIndex()
+        }
+
+    }
+
+    return (
+
+        <div className="profile-container">
+            <Header />
+
+            <h1>Casas cadastrados</h1>
+            <input
+                onChange={searchHouse}
+            />
+
+            <ul>
+                {houses.map(house => (
+                    <li key={house.id}>
+                        <strong>Casa: </strong>
+                        <p>{house.name}</p>
+                        
+                        <strong>Endereço: </strong>
+                        <p><FiMap /> {house.city} {house.uf}</p>
+
+                        <strong>Contato: </strong>
+                        <p><FiMail /> {house.email}</p>
+                        <p><FiPhone /> {house.cellNumber}</p>
+                    </li>
+                ))}
+            </ul>
+
+
+
+        </div>
+    )
+}
